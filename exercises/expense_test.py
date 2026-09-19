0eed06ca010d679080502fcfc679677db4a57f67
Sohaib <sohaibhabab9@gmail.com>
2026-09-19 17:19:35 +0100

    Add first Python expense exercise

---
+++ exercises/expense_test.py
@@ -0,0 +1,18 @@
+expense1_name = input("entrer le nom de l'expense 1: ")
+price1 = float(input("entrer le prix de l'expense 1: "))
+category1 = input("entrer la categorie de l'expense 1: ")
+
+print(f"Expense: {expense1_name}")
+print(f"Price: {price1} MAD")
+print(f"Category: {category1}")
+
+expense2_name = input("entrer le nom de l'expense 2: ")
+price2 = float(input("entrer le prix de l'expense 2: "))
+category2 = input("entrer la categorie de l'expense 2: ")
+
+print(f"Expense: {expense2_name}")
+print(f"Price: {price2} MAD")
+print(f"Category: {category2}")
+
+somme_expenses = price1 + price2
+print(f"Total expenses:{somme_expenses} MAD")
